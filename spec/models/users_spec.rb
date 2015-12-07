7b9ffc5e11d64746bf5dad932cc241a0524ec0c6
Matt Leo <email": null>
2015-12-07 17:27:04 -0600

    add tests for users model

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -3,6 +3,10 @@ require 'rails_helper'
 describe User do
   let(:user) { User.new }
   describe 'User' do
-    it { is_expected.to validate_presence_of(:username) }
+    # it { is_expected.to validate_presence_of(:username) }
+
+    it { should have_many(:approved_edits).with_foreign_key('approved_by_id') }
+    it { should have_many(:edits).with_foreign_key('author_id') }
+    it { should have_many(:created_articles).class_name('Article') }
   end
 end
