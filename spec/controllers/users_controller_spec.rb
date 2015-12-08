095ba9f4289f8e041c15a1b6047b9d7b163be2da
George & Matt Leo <pair+gaw1990.mleo5555@devbootcamp.com>
2015-12-08 14:34:43 -0600

    add more user controller tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -9,16 +9,28 @@ RSpec.describe UsersController, type: :controller do
     end
   end
 
-  # describe 'POST #create' do
-  #   it "creates a new user in the database" do
-  #     expect{
-  #       post :create, user: User.create(username: "test", email: "test@test.com",password_digest: "password")
-  #     }.to change(User,:count).by(1)
-  #   end
-  #
-  #   it "redirects to root path after successful user creation" do
-  #     # it { should redirect_to(action: :new) }
-  #   end
-  # end
+  describe 'POST #create' do
+    it "creates a new user in the database with valid credentials" do
+      expect {
+        post :create, {user: {username: "test", email: "test@test.com",password: "password"}}
+      }.to change(User,:count).by(1)
+    end
+
+    it "does not create new user in database with invalid credentials" do
+      expect {
+        post :create, {user: {username: "test", email: "incorrectemailformat",password: "password"}}
+      }.to_not change(User,:count)
+    end
+
+    it "redirects to root path after successful user creation" do
+        post :create, {user: {username: "test", email: "test@test.com",password: "password"}}
+      expect(:create).to redirect_to root_url
+    end
+
+    it "renders users/new after unsuccessful user creation" do
+        post :create, {user: {username: "test", email: "bademail",password: "password"}}
+      expect(:create).to render_template("user/new")
+    end
+  end
 
 end
