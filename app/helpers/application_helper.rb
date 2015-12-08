712efcb4f1bffddc803f76fb12de44756e782938
Marcus Davis <email": null>
2015-12-08 10:28:58 -0600

    Add admin helpers in ApplicationHelper and tests for them

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    current_user != nil
+  end
+
+  def is_admin?
+    if current_user.admin
+      return true
+    else
+      return false
+    end
+  end
 end
