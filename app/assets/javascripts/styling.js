63b1c4a75b949e45097665eba797b99aea749c26
[01;31m[K"name"[m[K: "Christopher \"Aji\" Slater <[01;31m[K"email"[m[K: null>
2015-12-08 15:44:17 -0600

    adds multiple featured articles on the front page ability

--- app/assets/javascripts/styling.js
+++ app/assets/javascripts/styling.js
@@ -1,12 +1,30 @@
 $(function() {
 
-
-  if ($(window).width() > 500) {
-    containerHeight = $("#container").height();
-    containerHeight -= 100;
-    $("#sidebar").css("min-height", containerHeight);
-  } else {
-    $("#sidebar").css("min-height", "4em");
+  function reSidebar(){
+    if ($(window).width() > 500) {
+      var containerHeight = $("#container").height();
+      containerHeight -= 100;
+      $("#sidebar").css("min-height", containerHeight);
+    } else {
+      $("#sidebar").css("min-height", "4em");
+    }
   }
 
-})
+  reSidebar();
+
+  $("#next-feature").on("click", function(event){
+    event.preventDefault();
+    var grabbedId = $(".feature-id-grabber").attr("id");
+    var data = {current_id: grabbedId};
+    var request = $.get(
+      url = "/index/next",
+      data = data
+    );
+    request.done( function(htmlResponse){
+      $("#feature-box").html(htmlResponse);
+      reSidebar();
+    });
+
+  });
+
+});
--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,12 +1,38 @@
 class IndexController < ApplicationController
   def index
+    @first = true
     @featured = Edit.where(:featured => true)[0]
     session[:feature_id] = 0
   end
 
   def next
-    feature_id =
-    @featured = Edit.where(:featured => true)[feature_id]
+    feature_id = (params["current_id"]).to_i
+    queried_features = Edit.where(:featured => true)
+    @featured = queried_features[feature_id]
+    p @featured
+    @first = first_feature?(queried_features)
+    @last = last_feature?(queried_features)
+    if request.xhr?
+      render 'index/_feature', layout: false
+    end
+  end
+
+  private
+
+  def last_feature?(queried_array)
+    if @featured.id == queried_array.length
+      return true
+    else
+      return false
+    end
+  end
+
+  def first_feature?(queried_array)
+    if @featured.id == 0
+      return true
+    else
+      return false
+    end
   end
 
 end
