0e8e73974e72d203dfc7a60c732f7e591648ef29
George & Matt Leo <pair+gaw1990.mleo5555@devbootcamp.com>
2015-12-08 09:56:24 -0600

    user create, login, logout working, no tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+
   root :to => 'users#new'
   post '/users' => 'users#create'
   get '/signup' => 'users#new'
+  get '/login' => 'sessions#new'
+  post '/sessions' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
