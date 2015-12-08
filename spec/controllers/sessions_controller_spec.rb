64f12d94a1138594dec3a8c9a87c48640b85607b
gaw1990 <georgewambold@gmail.com>
2015-12-08 11:24:25 -0600

    sesion basic routing/http response tests in place

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,11 +2,25 @@ require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
 
-  describe "GET #new" do
+  describe "GET '/login'" do
     it "returns http success" do
       get :new
       expect(response).to have_http_status(:success)
     end
+
+    it "routes to sessions#new" do
+     assert_routing({ method: 'get', path: '/login' }, { controller: "sessions", action: "new" })
+    end
+
+  end
+
+  describe "POST '/sessions'" do 
+
+    it "routes to sessions#create" do
+     assert_routing({ method: 'post', path: '/sessions' }, { controller: "sessions", action: "create" })
+    end
+
+
   end
 
   describe "GET #destroy" do
@@ -14,6 +28,13 @@ RSpec.describe SessionsController, type: :controller do
       get :destroy
       expect(response).to have_http_status(302)
     end
+
+    it "routes to sessions#create" do
+     assert_routing({ method: 'get', path: '/logout' }, { controller: "sessions", action: "destroy" })
+    end
+
   end
+
+  
 
 end
