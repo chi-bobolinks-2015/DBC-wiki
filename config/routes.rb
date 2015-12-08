d6bb457b323a52bae432e7ff5da06a7dec649e76
[01;31m[K"name"[m[K: "Christopher \"Aji\" Slater <[01;31m[K"email"[m[K: null>
2015-12-08 10:43:51 -0600

    layout and styling

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,4 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  root 'index#index' #just a test action for working on html
-
-
 end
