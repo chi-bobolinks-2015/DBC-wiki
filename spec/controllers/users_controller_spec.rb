e08fc19a9523eafe915b9c590abde2a38369ba88
George & Matt Leo <pair+gaw1990.mleo5555@devbootcamp.com>
2015-12-08 13:42:34 -0600

    add user controller tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -9,4 +9,16 @@ RSpec.describe UsersController, type: :controller do
     end
   end
 
+  # describe 'POST #create' do
+  #   it "creates a new user in the database" do
+  #     expect{
+  #       post :create, user: User.create(username: "test", email: "test@test.com",password_digest: "password")
+  #     }.to change(User,:count).by(1)
+  #   end
+  #
+  #   it "redirects to root path after successful user creation" do
+  #     # it { should redirect_to(action: :new) }
+  #   end
+  # end
+
 end
