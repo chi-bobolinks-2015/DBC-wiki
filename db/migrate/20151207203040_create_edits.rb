d97435a3f82ad631440cd7c7b8e2ac46dec7fb2e
Matt Leo <email": null>
2015-12-07 18:07:35 -0600

    add constraints to migrations

--- db/migrate/20151207203040_create_edits.rb
+++ db/migrate/20151207203040_create_edits.rb
@@ -1,11 +1,11 @@
 class CreateEdits < ActiveRecord::Migration
   def change
     create_table :edits do |t|
-      t.integer :article_id
-      t.integer :author_id
+      t.integer :article_id, null: false
+      t.integer :author_id, null: false
       t.boolean :approved
-      t.string :content
-      t.string :title
+      t.string :content, null: false
+      t.string :title, null: false
       t.integer :approver_id
 
       t.timestamps null: false
--- db/migrate/20151207203336_create_articles.rb
+++ db/migrate/20151207203336_create_articles.rb
@@ -1,8 +1,8 @@
 class CreateArticles < ActiveRecord::Migration
   def change
     create_table :articles do |t|
-      t.integer :category_id
-      t.integer :user_id
+      t.integer :category_id, null: false
+      t.integer :user_id, null: false
 
       t.timestamps null: false
     end
