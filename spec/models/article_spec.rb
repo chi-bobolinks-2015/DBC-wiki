a053e85c6563b3d3685f4759394940fec21ddd98
irinarenteria <irina.ilascu@gmail.com>
2015-12-08 08:38:51 -0600

    Finish validations and leave factory_girl for now

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,17 +2,17 @@ require 'rails_helper'
 
 describe Article do
 
-	it 'has a valid factory' do
-		expect(build(:article)).to be_valid
-	end
+	# it 'has a valid factory' do
+	# 	expect(build(:article)).to be_valid
+	# end
 
-	let(:ruby) { build(:article) }
+	# let(:ruby) { build(:article) }
 
-	describe "ActiveModel validations" do
+	# describe "ActiveModel validations" do
 
-		it { expect(ruby).to validate_presence_of(:category_id) }
-		# it { expect(ruby).to validate_presence_of(:user_id) }
-	end
+	# 	it { expect(ruby).to validate_presence_of(:category_id) }
+	# 	# it { expect(ruby).to validate_presence_of(:user_id) }
+	# end
 	
 	it { should belong_to(:category) }
 	it { should have_many(:edits) }
--- spec/models/edit_spec.rb
+++ spec/models/edit_spec.rb
@@ -2,17 +2,17 @@ require 'rails_helper'
 
 describe Edit do
 
-	it 'has a valid factory' do
-		expect(build(:edit)).to be_valid
-	end
+	# it 'has a valid factory' do
+	# 	expect(build(:edit)).to be_valid
+	# end
 
-	let(:version) { build(:edit) }
+	# let(:version) { build(:edit) }
 
-	describe "ActiveModel validations" do
+	# describe "ActiveModel validations" do
 
-		it { expect(version).to validate_presence_of(:article_id) }
-		# it { expect(version).to validate_presence_of(:author_id) }
-	end
+	# 	it { expect(version).to validate_presence_of(:article_id) }
+	# 	# it { expect(version).to validate_presence_of(:author_id) }
+	# end
 	
 	it { should belong_to(:article) }
 end
