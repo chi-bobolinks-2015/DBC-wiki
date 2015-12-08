00ed0437e2b66879c16fdb8035e9c3d803f207c0
gaw1990 <georgewambold@gmail.com>
2015-12-07 22:00:21 -0600

    created user new/create views, some more auth code

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ActiveRecord::Base
 
   validates :username,:email, presence: true
   validates :email, uniqueness: true, format: { with: /\A.+@.+\..+\z/ }
+
+  validates :password, length: { minimum: 1 }
   validates_presence_of :password, :on => :create
-
-
   attr_accessible :email, :password, :password_confirmation
   has_secure_password
 end
