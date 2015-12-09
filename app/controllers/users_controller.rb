bd42d56d4c6fbee1e4f0a2af4575d5184d33f75d
gaw1990 <georgewambold@gmail.com>
2015-12-09 15:13:35 -0600

    Add auth to POST '/users'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,12 +39,16 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find_by(id: params[:id])
-    if @user.update_attributes(username: user_params[:username], email: user_params[:email], password: user_params[:password])
-      # flash[:success] = "Profile updated"
-      redirect_to "/"
-    else
-      @errors = @user.errors.full_messages
-      render 'users/edit'
+    if @user == current_user
+      if @user.update_attributes(username: user_params[:username], email: user_params[:email], password: user_params[:password])
+        # flash[:success] = "Profile updated"
+        redirect_to "/"
+      else
+        @errors = @user.errors.full_messages
+        render 'users/edit'
+      end
+    else 
+      redirect_to login_path 
     end
   end
 
