6208f8336566c02eb9a6e3e63d05e10289491528
irinarenteria <irina.ilascu@gmail.com>
2015-12-09 14:25:27 -0600

    Merge conflicts in edit controller and routes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,25 @@
 class ArticlesController < ApplicationController
 
-	def new
+	def index
+	end
 
+	def new
+		@category = Category.find(params[:category_id])
+		@edit = Edit.new
 	end
 
 	def show
-		article = Article.find(params[:id])
-		@edit = Edit.where(article_id: article.id, approved: true)
+		@article = Article.find(params[:id])
+		# @edit = Edit.where(article_id: article.id, approved: true)
     	render 'show'
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@category = Category.find(@article.category_id)
+		@article.destroy
+
+		redirect_to categories_path(@category)
 	end
 
 end
